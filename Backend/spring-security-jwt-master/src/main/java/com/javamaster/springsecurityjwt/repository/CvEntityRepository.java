5bf1d2199f6dbeeb92e22082b21eecc4ae55e004
Emirhan <emirhan.ulker@cmvteknoloji.com>
2021-10-15 12:37:31 +0300

    test proje's finish

---
+++ Backend/spring-security-jwt-master/src/main/java/com/javamaster/springsecurityjwt/repository/CvEntityRepository.java
@@ -0,0 +1,21 @@
+package com.javamaster.springsecurityjwt.repository;
+
+import com.javamaster.springsecurityjwt.config.CustomUserDetails;
+import com.javamaster.springsecurityjwt.entity.CvEntity;
+import com.javamaster.springsecurityjwt.entity.UserEntity;
+import com.javamaster.springsecurityjwt.entity.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CvEntityRepository extends JpaRepository<CvEntity, Long> {
+
+    @Query("select c from CvEntity c where c.user_id =:userEntity")
+    List<CvEntity> findAllByUser_id(UserEntity userEntity);
+
+
+
+}
